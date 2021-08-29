86f55e48c129168ad19bf3005c3c494fad0a1a16
Wahab Alamin <wahabalamin@gmail.com>
2021-08-29 08:28:15 -0400

    updated file to contain an alarm that will beep after a specified amount of time has been passed. The time and amount of alarms depends on the number(s) passed using command line arguments

---
+++ timer1.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2)
+
+const alarm = function(args) {
+  for (let i = 0; i < args.length; i ++) {
+    if (args[i] > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, args[i] * 1000);
+    }
+  }
+}
+
+alarm(args);
